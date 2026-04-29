3a8837a23fd4b4c5c0fa119fbf9bec20078d894e
Araz Nebizade <araznebiza@gmail.com>
2026-04-29 20:59:53 +0400

    Implement Queueing

--- src/main/java/com/team54/airportdispatchtycoonteam54/core/Queue/FlightRequest.java
+++ src/main/java/com/team54/airportdispatchtycoonteam54/core/Queue/FlightRequest.java
@@ -1,14 +1,18 @@
 package com.team54.airportdispatchtycoonteam54.core.Queue;
 
-
+import com.team54.airportdispatchtycoonteam54.core.Planes.Aircraft;
 
 public class FlightRequest {
 
 
-    
+    Aircraft aircraft;
 
     public FlightRequest(){
+        aircraft = FlightGenerator.generateRandom();
+    }
 
+    public Aircraft getAircraft() {
+        return aircraft;
     }
     
 }
--- src/main/java/com/team54/airportdispatchtycoonteam54/core/Queue/Queue.java
+++ src/main/java/com/team54/airportdispatchtycoonteam54/core/Queue/Queue.java
@@ -1,13 +1,29 @@
 package com.team54.airportdispatchtycoonteam54.core.Queue;
 
+import java.util.LinkedList;
+
+import com.team54.airportdispatchtycoonteam54.core.Planes.Aircraft;
+
 public class Queue {
     private static int currentID=0;
 
 
+    static LinkedList<Aircraft> aircraftQueue = new LinkedList<>();
 
 
     static int getNextID(){
         currentID++;
         return currentID;
     }
+
+
+    public static Aircraft getNextAircraft(){
+        Aircraft nextAircraft = aircraftQueue.removeFirst();
+        return nextAircraft;
+    }
+
+    public static LinkedList<Aircraft> getAircraftQueue(){
+        return aircraftQueue;
+    }
+
 }
