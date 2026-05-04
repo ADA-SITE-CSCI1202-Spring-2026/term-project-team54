df7028c3cd51f47ad3215cca22f099da174c3c81
Araz Nebizade <araznebiza@gmail.com>
2026-05-04 22:12:41 +0400

    Add JAR compilation

--- src/main/java/com/team54/airportdispatchtycoonteam54/core/Queue/FlightRequest.java
+++ src/main/java/com/team54/airportdispatchtycoonteam54/core/Queue/FlightRequest.java
@@ -3,20 +3,50 @@ package com.team54.airportdispatchtycoonteam54.core.Queue;
 import java.util.Random;
 import com.team54.airportdispatchtycoonteam54.core.Planes.*;
 
+
+
+
+
+
+
+
 public class FlightRequest {
+
+
     Aircraft aircraft;
 
-    public FlightRequest(AircraftType type){
-        aircraft = AircraftFactory.create(type);
+    public FlightRequest(){
+        aircraft = FlightGenerator.generateRandom();
     }
 
     public Aircraft getAircraft() {
         return aircraft;
     }
+    
+    
+    private class FlightGenerator {
 
 
-    @Override
-    public String toString() {
-        return "IMPLEMENT TO STRING"; // TODO:
+        private static final Random random = new Random();
+
+        public static Aircraft generateRandom(){
+            AircraftType type = AircraftType.values()[random.nextInt(AircraftType.values().length)];
+            
+            
+            
+            int neededFuel = random.nextInt(200, 1000);
+            int neededMeals = random.nextInt(10, 100);
+            int neededLuggage = random.nextInt(10,200);
+
+            AircraftNeeds needs = new AircraftNeeds(neededFuel,neededMeals,neededLuggage);
+
+            Aircraft generatedAircraft =  (Aircraft)AircraftFactory.create(type);
+            
+            
+
+            return generatedAircraft;
+        }
+
     }
+
 }
